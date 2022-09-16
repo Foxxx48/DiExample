83ae7f36073728a5241b6b1e0eb06a6d09f41e63
Foxxx48 <Foxxdim@mail.ru>
2022-09-16 11:58:04 +0300

    Initial commit Add Dagger2 dependencies

---
+++ app/src/main/java/com/fox/diexample/ComputerTower.kt
@@ -0,0 +1,7 @@
+package com.fox.diexample
+
+class ComputerTower (
+    val storage: Storage,
+    val memory: Memory,
+    val processor: Processor
+        )
---
+++ app/src/main/java/com/fox/diexample/Processor.kt
@@ -0,0 +1,4 @@
+package com.fox.diexample
+
+class Processor {
+}
